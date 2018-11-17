3777203faf6684547b3b4086562508a6e73df920
Paola Arredondo <arredondop03@gmail.com>
2018-11-16 22:24:27 -0500

    updated service

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http'; 
+import { Observable } from 'rxjs'
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  handleError(e){ //e comes magically and is whatever the error is.
+    console.log(e)
+    return Observable.throw(e.json().message)
+  }
+
+
+
+
+
+
 }

2d0b20137655c1d9239052d8d7e1b8a63edd5613
Paola Arredondo <arredondop03@gmail.com>
2018-11-30 16:58:01 -0500

    added fields for the signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }          from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-
-//Components-----
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';

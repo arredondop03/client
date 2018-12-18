d54c7fa41e44fe76e6855bc801ee6e12b0f99426
Paola Arredondo <arredondop03@gmail.com>
2018-12-18 11:23:24 -0500

    added dropdown for state

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -12,9 +12,20 @@ export class SignupComponent implements OnInit {
   theActualuser:any = {}
   theError:any
 
+  states: string[] = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 
+  'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 
+  'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 
+  'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 
+  'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
+  'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 
+  'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 
+  'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 
+  'WI', 'WY']
+
   constructor(private authService: AuthServiceService) { }
 
   signup(){
+
     this.authService.signup(this.signupUser)
     .subscribe(
       userObjFromApi =>{this.successCallback(userObjFromApi)},
